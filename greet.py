6e81fed116503ffccb0e67bcfa1b5174b0ea86e3
aditya <adityay186@gmail.com>
2021-04-27 16:26:30 +0530

    First Commit

---
+++ greet.py
@@ -0,0 +1,16 @@
+from tts import speak
+
+def greet():
+    import datetime
+    hour = int(datetime.datetime.now().hour)
+    if hour>= 0 and hour<12:
+        speak("Good Morning Sir !")
+  
+    elif hour>= 12 and hour<18:
+        speak("Good Afternoon Sir !")  
+  
+    else:
+        speak("Good Evening Sir !") 
+  
+    speak("I am your Assistant")
+    speak("How may I help you?")
---
+++ news.py
@@ -0,0 +1,32 @@
+import requests
+import json
+from tts import *
+
+
+def playNewsIndia():
+    r = requests.get("https://newsapi.org/v2/everything?q=india&from=2021-04-27&sortBy=publishedAt&apiKey=f8c963cf1fcf46c8bac952f272bc0da0")
+
+    news_data=r.json()
+    # print(news_data)
+    l=len(news_data["articles"])
+
+    for i in range(1):
+        k=news_data["articles"][i]["description"]
+        source=news_data["articles"][i]["source"]["name"]
+        if k!=None:
+            speak(source+"से अगली खबर")
+            speak(k)
+
+def playNews():
+    r = requests.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=f8c963cf1fcf46c8bac952f272bc0da0")
+
+    news_data=r.json()
+    # print(news_data)
+    l=len(news_data["articles"])
+
+    for i in range(1):
+        k=news_data["articles"][i]["description"]
+        source=news_data["articles"][i]["source"]["name"]
+        if k!=None:
+            speak("News From :"+source)
+            speak(k)
