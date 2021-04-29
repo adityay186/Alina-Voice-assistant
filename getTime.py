0a4624b050a685f8af31a611ca9e2820634c2479
aditya <adityay186@gmail.com>
2021-04-29 11:21:32 +0530

    time Fixes

--- getTime.py
+++ getTime.py
@@ -1,21 +1,19 @@
 
 import time
+import datetime
 # get current time
-date_time1 = time.strftime("%-I")
-date_time2 = time.strftime("%M")
-date_time3= time.strftime("%p")
+# date_time1 = time.strftime("%-I")
+# date_time2 = time.strftime("%M")
+# date_time3= time.strftime("%p")
 from tts import *
 
 # print(date_time1,date_time2,date_time3)
 
-def getTime(x,y,z):
-    h=x
-    m=y
-    ampm=z
+def getTime():
     val=None
-    # h=date_time1 = time.strftime("%-I")
-    # m=date_time2 = time.strftime("%M")
-    # ampm=date_time3= time.strftime("%p")
+    h=date_time1 = time.strftime("%-I")
+    m=date_time2 = time.strftime("%M")
+    ampm=date_time3= time.strftime("%p")
     if ampm=="PM":
         if int(h)==12 and int(m)==0:
             val="Noon"
@@ -36,5 +34,21 @@ def getTime(x,y,z):
             val="Night"
         else:
             val="Morning"
+    strTime = time.strftime("%-I:%M")
+    print(strTime)
     if val=="Noon":
-        speak("The time is "+" 12 "+" noon")
+        speak("The time is "+strTime+" in the noon")
+    elif val=="Afternoon":
+        speak("The time is "+strTime+" in the afternoon")
+    elif val=="Evening":
+        speak("The time is "+strTime+" in the evening")
+    elif val=="Night":
+        speak("The time is "+strTime+" at night")
+    elif val=="Mid-Night":
+        speak("The time is "+strTime+" at mid-night")
+    elif val=="Morning":
+        speak("The time is "+strTime+" in the morning")
+getTime()
+
+   
+# speak(f"Sir, the time is {strTime}")
--- tts.py
+++ tts.py
@@ -1,7 +1,7 @@
 def speak(text_to_speak):
     from gtts import gTTS
     import os 
-    language="hi"
+    language="en"
     myobj = gTTS(text=text_to_speak, lang=language, slow=False)
     myobj.save("speak.mp3")
     os.system("play "+"speak.mp3"+" tempo 1.4")
