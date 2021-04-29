499aa088e85caee0d6f4d8c4b386ee1cc2d065bb
aditya <adityay186@gmail.com>
2021-04-29 18:07:45 +0530

    Random 2

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from listen import *
 from mainListen import *
 import os
 
-# speak("The weather in "+city+state+" currently is "+str(current)+" degree celsius with an high of "+str(high)+" degree celsius.")
-
 # prerequisite section gathering data beforehand so that it can produce results faster when required
 geodata=getLocation()
 city=geodata[0]
