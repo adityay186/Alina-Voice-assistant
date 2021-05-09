910d58d8fc7a4abdacc7eeda476314cae8e6805a
aditya <adityay186@gmail.com>
2021-05-09 08:35:56 +0530

    Tweaks..

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from news import *
 from listen import *
 from mainListen import *
 import os
+import threading
 
 # prerequisite section gathering data beforehand so that it can produce results faster when required
-geodata=getLocation()
-try:
-    city=geodata[0]
-    state=geodata[1]
-except TypeError:
-    print("Empty")
-else:
-    print("Location :"+city+", "+state)
+def pr():
+    geodata=getLocation()
+    try:
+        city=geodata[0]
+        state=geodata[1]
+    except TypeError:
+        print("Empty")
+    else:
+        print("Location :"+city+", "+state)
+
+done=False
+t=threading.Thread(target=pr)
+t.start()
 if __name__ == '__main__':
     clear = lambda: os.system('cls')
     clear()
